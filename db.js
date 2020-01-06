f8830b7b0238e26bee39c463918cfb5e666c78ef
Dario Mercado <28830113+dmarket7@users.noreply.github.com>
2020-01-05 17:47:42 -0800

    adding DB_URI variable to pg connection

--- db.js
+++ db.js
@@ -2,9 +2,10 @@
 
 
 const { Client } = require("pg");
+const { DB_URI } = require("./config");
 
 const client = new Client({
-  connectionString: "postgresql:///cashapp"
+  connectionString: DB_URI
 });
 
 client.connect();
