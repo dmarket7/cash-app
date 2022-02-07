d3284be5ad52d20073005ca3cab8fd941c840490
Dario Mercado <28830113+dmarket7@users.noreply.github.com>
2022-02-07 09:37:26 -0800

    Revert "changing pg config to fix connection"
    This reverts commit 7224d1872cbd3facde193f47b50e128deae471a2.

--- db.js
+++ db.js
@@ -5,15 +5,7 @@ const { Client } = require("pg");
 const { DB_URI } = require("./config");
 
 const client = new Client({
-  connectionString: DB_URI,
-  options: { 
-    dialect: "postgres",
-    native: true,
-    ssl: true, 
-    dialectOptions: {
-      ssl: true
-    }
-  }
+  connectionString: DB_URI
 });
 
 client.connect();
